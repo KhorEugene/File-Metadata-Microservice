8a44dd6620bd14c7a7662f42121161f19a161072
Khor Eugene <khor3ug3n3@yahoo.com>
2019-04-18 15:56:34 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+// require and use "multer"...
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+var app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+app.post('/api/fileanalyse',upload.single('upfile'),function(req,res,next){
+  const obj = {'filename':req.file.originalname,'size':req.file.size+' bytes','type':req.file.mimetype};
+  res.json(obj);
+  console.log('Upload successful.');
+});
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
